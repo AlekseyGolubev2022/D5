0ec41480b31ac08426b79b6f73af544dde1bdb74
Aleksey Golubev <alekseygolubev@mail.ru>
2023-03-30 06:44:58 +0300

    Add files via upload

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from .views import NewsList, PostDetail
 
-#app_name = 'news'
+app_name = 'news'
 
 urlpatterns = [
    path('', NewsList.as_view()),
-   path('<int:pk>', PostDetail.as_view()),
+   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
 ]
