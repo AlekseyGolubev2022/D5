874861ab873502246e18220432900c055b83eceb
Aleksey Golubev <alekseygolubev@mail.ru>
2023-04-06 07:47:51 +0300

    Add files via upload

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -5,6 +5,7 @@ from .views import NewsList, PostDetail
 app_name = 'news'
 
 urlpatterns = [
-   path('', NewsList.as_view()),
-   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('', NewsList.as_view()),
+    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('search', NewsList.as_view(), name='news_search'),
 ]
